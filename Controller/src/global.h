e757148fb3926d6e3e5e7f52443b75918de121eb
unknown <birkelandmarcus@gmail.com>
2019-02-16 12:27:55 +0100

    Added RF functionality. Made separate folders for control and car

--- Controller/src/global.h
+++ Controller/src/global.h
@@ -11,6 +11,9 @@
 #define analogY_pin 7
 #define stickButton 2
 
+RF24 radio (7,8);
+const byte address[6] = "00001";
+
 struct controller {
     bool buttonA=0, buttonB=0, buttonX=0, buttonY=0,buttonShoulder=0;
     int stick_xVal;
--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,17 +1,25 @@
 #include <Arduino.h>
 #include <stdio.h>
+#include <SPI.h>
+#include <RF24.h>
 #include "global.h"
 const bool DEBUG = 1;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(stickButton , INPUT_PULLUP); // Setup all buttons
+  Serial.begin(9600);
+  // Setup radio
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+
+  // Setup all buttons
+  pinMode(stickButton , INPUT_PULLUP); 
   pinMode(buttonA_pin , INPUT_PULLUP);
   pinMode(buttonB_pin , INPUT_PULLUP);
   pinMode(buttonX_pin , INPUT_PULLUP);
   pinMode(buttonY_pin , INPUT_PULLUP);
   pinMode(buttonShoulder_pin , INPUT_PULLUP);
-  Serial.begin(9600);
 }
 
 void loop() {
@@ -91,5 +99,6 @@ char* setOutput (struct controller* c1){
 }
 
 int sendOutput (char* output) {
-
+  radio.write(&output, sizeof(output));
+  delay(1000); 
 }  
